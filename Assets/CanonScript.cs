a58919948cc3b8481ca6cc195b6b33585ab24140
Matheus Palheta <matheuspb95@gmail.com>
2016-10-27 09:58:50 -0400

    Ship, Cat, Bullets

---
+++ Assets/CanonScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanonScript : MonoBehaviour {
+    public float duration, fromAngle, toAngle;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(ShootRotating());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator ShootRotating()
+    {
+        GetComponent<ShootScript>().CanShoot = true;
+        transform.rotation = Quaternion.Euler(0, 0, fromAngle);
+        float time = 0;
+        while(time <= duration)
+        {
+            time += Time.deltaTime;
+            transform.rotation = Quaternion.Lerp(Quaternion.Euler(0, 0, fromAngle), Quaternion.Euler(0, 0, toAngle), time / duration);
+            yield return new WaitForEndOfFrame();
+        }
+        GetComponent<ShootScript>().CanShoot = false;
+    }
+}
